28d306550bbefe88f80e288908b9bb302d3d912f
Denys Balako <denys.balako@gmail.com>
2026-05-02 21:28:22 +0300

    feat: init spring boot 4 project with gradle kotlin dsl

---
+++ src/test/java/io/github/denisbalako/bittorrent_client/BittorrentClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.denisbalako.bittorrent_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BittorrentClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
